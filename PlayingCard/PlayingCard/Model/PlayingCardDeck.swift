1e41c6af6494f333994b2913c230cdc9c267d1ec
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-09-28 20:03:04 +0100

    Added init() Method
    Added array of cards and init() method to populate it

--- PlayingCard/PlayingCard/Model/PlayingCardDeck.swift
+++ PlayingCard/PlayingCard/Model/PlayingCardDeck.swift
@@ -9,4 +9,14 @@
 import Foundation
 
 struct PlayingCardDeck {
+    private(set) var cards = [PlayingCard]()
+    
+    init() {
+        for suit in PlayingCard.Suit.all {
+            for rank in PlayingCard.Rank.all {
+                cards.append(PlayingCard(suit: suit, rank: rank))
+            }
+        }
+    }
+    
 }
