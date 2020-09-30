50eaec048eb5a2bd06a14d9a06ab8c8105fc4d5c
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-09-30 19:29:21 +0100

    Added Int Extension
    Added Int extension with computed property arc4random

--- PlayingCard/PlayingCard/Model/PlayingCardDeck.swift
+++ PlayingCard/PlayingCard/Model/PlayingCardDeck.swift
@@ -28,4 +28,14 @@ struct PlayingCardDeck {
     }
 }
 
+extension Int {
+    var arc4random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(self)))
+        } else {
+            return 0
+        }
+    }
 }
