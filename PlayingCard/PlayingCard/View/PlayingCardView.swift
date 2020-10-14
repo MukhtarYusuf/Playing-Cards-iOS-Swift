c0e747b09015219c976e6fb6b849b55039eb66e4
MukhtarYusuf <mukhtar.yusuf@aun.edu.ng>
2020-10-14 22:04:03 +0100

    Override layoutSubViews()
    Overrid func layoutSubviews() to reposition corner labels

--- PlayingCard/PlayingCard/View/PlayingCardView.swift
+++ PlayingCard/PlayingCard/View/PlayingCardView.swift
@@ -119,4 +119,19 @@ class PlayingCardView: UIView {
         setNeedsLayout()
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        configureCornerLabel(upperLeftCornerLabel)
+        upperLeftCornerLabel.frame.origin = bounds.origin.offSetBy(dx: cornerOffset, dy: cornerOffset)
+        
+        configureCornerLabel(lowerRightCornerLabel)
+        lowerRightCornerLabel.transform = CGAffineTransform.identity
+            .translatedBy(x: lowerRightCornerLabel.frame.size.width, y: lowerRightCornerLabel.frame.size.height)
+            .rotated(by: CGFloat.pi)
+        lowerRightCornerLabel.frame.origin = CGPoint(x: bounds.maxX, y: bounds.maxY)
+            .offSetBy(dx: -cornerOffset, dy: -cornerOffset)
+            .offSetBy(dx: -lowerRightCornerLabel.frame.size.width, dy: -lowerRightCornerLabel.frame.size.height)
+    }
+
 }
